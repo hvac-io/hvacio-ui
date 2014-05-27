bc99f32e8ce011aae313060c4b5dcd048a391a2f
Frozenlock <frozenlock@gmail.com>
2014-05-26 23:38:50 -0400

    tidy up project.clj

--- project.clj
+++ project.clj
@@ -10,40 +10,21 @@
                  [reagent "0.4.2"]
                  [org.clojars.frozenlock/query "0.2.3"]
                  [historian "1.0.1"]
-                 ;[org.clojars.franks42/cljs-uuid-utils "0.1.3"]
                  [cljs-ajax "0.2.3"]
                  
                  ;; internationalization
-                 [com.taoensso/tower "2.1.0-RC1"]
-                 ]
+                 [com.taoensso/tower "2.1.0-RC1"]]
 
   :profiles {:dev {:dependencies [[ring "1.2.1"]
                                   [compojure "1.1.6"]
                                   
-                                  ;; for the cljs REPL
-                                  [com.cemerick/piggieback "0.1.3"]
-                                  
                                   ;; test API
-                                  [liberator "0.11.0"]
-
-                                  ;;cljs
-                                        ; [alandipert/storage-atom "1.2.3"]
-                                  ;[org.clojars.franks42/cljs-uuid-utils "0.1.3"]
-                                  ]
+                                  [liberator "0.11.0"]]
 
                    :main hvacio-ui.server
                    :ring {:handler hvacio-ui.server/app}
                    :source-paths ["src/clj"]
-                   ;; :injections [(ns user)
-                   ;;              (require '[cljs.repl.browser :as brepl]
-                   ;;                       '[cemerick.piggieback :as pb])
-                   ;;              (defn browser-repl []
-                   ;;                (pb/cljs-repl :repl-env
-                   ;;                              (brepl/repl-env :port 9000)))]
-                   ;; :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
-             }}
-
-;  :resource-paths ["resources/dict"]
+                   }}
 
   :plugins [[lein-cljsbuild "1.0.2"]
             [lein-ring "0.8.10"]]
@@ -57,7 +38,7 @@
                                          :optimizations :simple
                                          :pretty-print true
                                          :preamble ["reagent/react.min.js"]}
-                              ;:jar true
+                              ;:jar true ;---> no need for it, source-paths already configured
 }
                        :dev {:source-paths ["src/cljs" "src-dev/cljs"]
                              :compiler {:output-to "resources/public/js/cljs.js"
