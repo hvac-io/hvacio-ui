0a71fb82717300f9b59044b0f5270779c9d519d6
Frozenlock <frozenlock@gmail.com>
2014-05-29 23:47:54 -0400

    initial release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hvacio/hvacio-ui "0.1.0-SNAPSHOT"
+(defproject hvacio/hvacio-ui "0.1.0"
   :description "A common UI for multiple HVAC.IO applications."
   :url "https://hvac.io"
   :license {:name "GNU General Public License V3"
